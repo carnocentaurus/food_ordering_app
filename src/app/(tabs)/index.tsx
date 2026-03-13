5efffaef3d2a7c11fc9064b8316ebc01b95342ef
EJay <carnocentaurus@gmail.com>
2026-03-13 09:33:33 +0800

    added pizza img and basic container styles

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import Colors from '../constants/Colors';
 import products from '@/assets/data/products';
 
@@ -7,17 +7,24 @@ const product = products[0];
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
+      <Image source={{uri: product.image}} style={styles.image}/>
+
       <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>{product.price}</Text>
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 20
+  },
+
+  image: {
+    width: '100%',
+    aspectRatio: 1
   },
 
   title: {
