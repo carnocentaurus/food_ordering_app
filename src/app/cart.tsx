8d8f160e8172f52839a62cb2c1cfdd6ba29f398e
EJay <carnocentaurus@gmail.com>
2026-03-20 14:43:45 +0800

    added checkout button and total price logic

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,19 +1,24 @@
-import { View, Text, FlatList } from 'react-native';
+import { View, Button, FlatList, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { Platform } from 'react-native';
 import { useCart } from './providers/CartProvider';
 import CartListItem from './components/CartListItem';
 
 const CartScreen = () => {
-    const { items } = useCart();
+    const { items, total } = useCart();
 
     return(
-        <View>
+        <View style={{ padding: 10 }}>
             <FlatList 
                 data={items}
                 renderItem={({ item }) => <CartListItem cartItem={item} />}
-                contentContainerStyle={{ padding: 10, gap: 10 }}
+                contentContainerStyle={{ gap: 10 }}
             />
+
+            <Text style={{ marginTop: 20, fontSize: 20, fontWeight: '500' }}>
+                Total: ${total}
+            </Text>
+            <Button title='Checkout' />
 
             <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
         </View>
--- src/app/providers/CartProvider.tsx
+++ src/app/providers/CartProvider.tsx
@@ -6,12 +6,14 @@ type CartType = {
     items: CartItem[];
     addItem: (product: Product, size: PizzaSize) => void;
     updateQuantity: (itemId: string, amount: -1 | 1) => void;
+    total: number;
 };
 
 const CartContext = createContext<CartType>({
     items: [],
     addItem: () => {},
-    updateQuantity: () => {}
+    updateQuantity: () => {},
+    total: 0
 });
 
 const CartProvider = ({ children }: PropsWithChildren) => {
@@ -48,9 +50,13 @@ const CartProvider = ({ children }: PropsWithChildren) => {
         setItems(updatedItems);
     }
 
+    const total = items.reduce(
+        (sum, item) => (sum + item.product.price * item.quantity), 0
+    );
+
     return(
         <CartContext.Provider 
-            value={{ items, addItem, updateQuantity }}
+            value={{ items, addItem, updateQuantity, total }}
         >
             {children}
         </CartContext.Provider>
